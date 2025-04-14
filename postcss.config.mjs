7165b95927901183f7ddd47b9c5f8fd5c9aa8fa8
adityathodsare <thodsareaditya@gmail.com>
2025-04-14 11:03:41 +0530

    Updated code

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,7 @@
-const svgToDataUri = require("mini-svg-data-uri");
+import svgToDataUri from "mini-svg-data-uri";
+import colors from "tailwindcss/colors";
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 
-const colors = require("tailwindcss/colors");
-const {
-  default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
 const config = {
   plugins: ["@tailwindcss/postcss"],
 };
