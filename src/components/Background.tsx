750b57cf80450e6058073239135b6737bf031c28
adityathodsare <thodsareaditya@gmail.com>
2025-04-23 21:30:11 +0530

    Updated code

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -21,6 +21,16 @@ function Background() {
             </span>
           </button>
         </div>
+        <div className="flex justify-center mt-4">
+          <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 w-40">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-lg font-medium text-white backdrop-blur-3xl">
+              <Link href={"/tracking"} className="text-white text-lg">
+                track data
+              </Link>
+            </span>
+          </button>
+        </div>
       </h2>
     </BackgroundLines>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,85 +1,87 @@
 function Footer() {
-    return (
-      <footer className="bg-black text-gray-400 py-12">
-        <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8">
-          <div>
-            <h2 className="text-white text-lg font-semibold mb-4">About Us</h2>
-            <p className="mb-4">
-              we are team filled with sigma guys called each other bhau ( khushi bhau, kirti bhau( birthday girl),  aditya bhau ), so buy our product now... 
-            </p>
-          </div>
-          <div>
-            <h2 className="text-white text-lg font-semibold mb-4">Quick Links</h2>
-            <ul>
-              <li>
-                <a
-                  href="#"
-                  className="hover:text-white transition-colors duration-300"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="hover:text-white transition-colors duration-300"
-                >
-                 about us
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="hover:text-white transition-colors duration-300"
-                >
-                 buy now
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="hover:text-white transition-colors duration-300"
-                >contact us
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h2 className="text-white text-lg font-semibold mb-4">Follow Us</h2>
-            <div className="flex space-x-4">
+  return (
+    <footer className="bg-black text-gray-400 py-12">
+      <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8">
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">About Us</h2>
+          <p className="mb-4">
+            we are team filled with sigma guys called each other bhau ( khushi
+            bhau, kirti bhau, aditya bhau ), so buy our product now...
+          </p>
+        </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Quick Links</h2>
+          <ul>
+            <li>
               <a
                 href="#"
                 className="hover:text-white transition-colors duration-300"
               >
-                Facebook
+                Home
               </a>
+            </li>
+            <li>
               <a
                 href="#"
                 className="hover:text-white transition-colors duration-300"
               >
-                Twitter
+                about us
               </a>
+            </li>
+            <li>
               <a
                 href="#"
                 className="hover:text-white transition-colors duration-300"
               >
-                Instagram
+                buy now
               </a>
-            </div>
-          </div>
-          <div>
-            <h2 className="text-white text-lg font-semibold mb-4">Contact Us</h2>
-            <p>pune,maharashra,India</p>
-            <p>412216</p>
-            <p>Email: dev@teamSigma.com</p>
-            <p>Phone: (123) 456-7890</p>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="hover:text-white transition-colors duration-300"
+              >
+                contact us
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Follow Us</h2>
+          <div className="flex space-x-4">
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Facebook
+            </a>
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Twitter
+            </a>
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Instagram
+            </a>
           </div>
         </div>
-        <p className="text-center text-xs pt-8">
-          © 2025 TeamSigma. All rights reserved.
-        </p>
-      </footer>
-    );
-  }
-  
-  export default Footer;
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Contact Us</h2>
+          <p>pune,maharashra,India</p>
+          <p>412216</p>
+          <p>Email: thodsareaditya@gmail.com</p>
+          <p>Phone: 8263878470</p>
+        </div>
+      </div>
+      <p className="text-center text-xs pt-8">
+        © 2025 TeamSigma. All rights reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
